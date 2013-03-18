695ef4e8f8a256c26d2063283886d6bffba5a18b
Alan deLevie <adelevie@gmail.com>
2013-03-18 01:00:39 -0400

    changed comments

--- test/configuration.example.rb
+++ test/configuration.example.rb
@@ -1,7 +1,7 @@
 # usage:
 # require_relative "path/to/configuration.rb"
 # keys = Configuration.keys
-# keys["parse"]["application_id"]
+# keys["ironio"]["token"]
 
 module Configuration
   def self.keys
